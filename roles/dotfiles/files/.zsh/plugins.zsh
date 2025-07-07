93eeeab392466d53fc049b135445bafe0b4c3f74
Jussi Heikkilä <jussih@gmail.com>
2025-07-07 19:19:20 +0300

    source cargo config if exists

--- roles/dotfiles/files/.zsh/plugins.zsh
+++ roles/dotfiles/files/.zsh/plugins.zsh
@@ -1,3 +1,8 @@
 # FZF
 source /usr/share/doc/fzf/examples/key-bindings.zsh
 source /usr/share/doc/fzf/examples/completion.zsh
+
+# Cargo
+if [[ -e $HOME/.cargo/env ]] then
+    source "$HOME/.cargo/env"
+fi
